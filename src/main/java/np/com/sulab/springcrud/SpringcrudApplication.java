1270325b49dfa5f47413f29be1619bc066d04863
Sulabh Bartaula <sulabhbartaula@gmail.com>
2020-07-20 20:14:17 +1000

    Fist up and running index

---
+++ src/main/java/np/com/sulab/springcrud/SpringcrudApplication.java
@@ -0,0 +1,13 @@
+package np.com.sulab.springcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcrudApplication.class, args);
+    }
+
+}
---
+++ src/main/java/np/com/sulab/springcrud/contollers/IndexController.java
@@ -0,0 +1,17 @@
+package np.com.sulab.springcrud.contollers;
+
+/**
+ * Sulabh Bartaula
+ */
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+}
